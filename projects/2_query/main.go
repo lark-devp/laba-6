364cdedb0689c11723c272a7d7e8ae28cfe6aff3
lark-devp <botrazumniy@gmail.com>
2024-10-10 13:12:05 +0300

    query

--- projects/2_query/main.go
+++ projects/2_query/main.go
@@ -1,9 +1,24 @@
 package main
 
-// здесь надо написать код
+import (
+	"fmt"
 
-func main() {
-	// и здесь тоже
+	"net/http" // пакет для поддержки HTTP протокола
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	_, err := w.Write([]byte("Hello," + r.URL.Query().Get("name") + "!"))
+	if err != nil {
+		panic(err)
+	}
+
 }
+func main() {
+	// здесь ваш код
+	http.HandleFunc("/api/user", handler)
 
-// и тут тоже (если очень надо)
+	err := http.ListenAndServe(":9000", nil)
+	if err != nil {
+		fmt.Println("Ошибка запуска сервера: ", err)
+	}
+}
